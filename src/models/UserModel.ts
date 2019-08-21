725df23e6870f34d6f694fe8cced517b2a68c188
Lucas <l.duarte.mk@gmail.com>
2019-08-21 15:34:45 -0300

    user model end

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,5 +1,7 @@
 import * as Sequelize from 'sequelize'
+import { genSaltSync, hashSync, compareSync } from 'bcryptjs'
 import { BaseModelInterface } from '../interfaces/BaseModelInterface'
+import { ModelsInterface } from '../interfaces/ModelsInterface'
 
 export interface UserAttributes {
     id?: number
@@ -7,47 +9,65 @@ export interface UserAttributes {
     email?: string
     password?: string
     photo?: String
+    createdAt?: String
+    updatedAt?: String
 }
 
 export interface UserInstance extends Sequelize.Instance<UserAttributes>, UserAttributes {
     isPassword(encodedPassword: string, password: string): boolean
 }
 
-export interface UserModel extends BaseModelInterface, Sequelize.Model<UserInstance, UserAttributes> {}
+export interface UserModel extends BaseModelInterface, Sequelize.Model<UserInstance, UserAttributes> { }
 
 export default (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes): UserModel => {
-    const User: UserModel = 
-    sequelize.define('User', {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING(128),
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING(128),
-            allowNull: false,
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING(128),
-            allowNull: false,
-            validate: {
-                notEmpty: true
+    const User: UserModel =
+        sequelize.define('User', {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            name: {
+                type: DataTypes.STRING(128),
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING(128),
+                allowNull: false,
+                unique: true
+            },
+            password: {
+                type: DataTypes.STRING(128),
+                allowNull: false,
+                validate: {
+                    notEmpty: true
+                }
+            },
+            photo: {
+                type: DataTypes.BLOB({
+                    length: 'long'
+                }),
+                allowNull: true,
+                defaultValue: null
             }
-        },
-        photo: {
-            type: DataTypes.BLOB({
-                length: 'long'
-            }),
-            allowNull: true,
-            defaultValue: null
-        }
-    })
+        }, {
+                tableName: 'users',
+                hooks: {
+                    beforeCreate: (user: UserInstance, options: Sequelize.CreateOptions): void => {
+                        const salt = genSaltSync()
+                        user.password = hashSync(user.password, salt)
+                    }
+                }
+            })
+
+
+    //Para fazer uma associação
+    User.associate = (models: ModelsInterface) => { }
+
+    User.prototype.isPassword = (encodedPassword: string, password: string): boolean => {
+        return compareSync(password, encodedPassword)
+    }
 
     return User
 }
